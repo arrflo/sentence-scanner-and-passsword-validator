eb0f9f0a3164010295a1699969346549cee75742
romalynfloranda <romalynfloranda0203@gmail.com>
2021-11-28 20:14:34 +0800

    The program is working.

--- program1.py
+++ program1.py
@@ -9,7 +9,7 @@
 # consonants: 8
 
 def sentEnce():
-    _sentence = input("Enter a sentence here:")
+    _sentence = input("Insert an idea here (sentence): ")
     return _sentence
 
 def display (w,v,c):
@@ -19,6 +19,8 @@ def display (w,v,c):
 
 sentence = sentEnce ()
 
+
+#word count
 def numberWords (string):
     string1 = string.strip()
     count = 1 
@@ -27,22 +29,28 @@ def numberWords (string):
             count = count + 1
     return count
 
+
+#vowels
 def numberVowels (string):
     string2 = string.strip()
     count = 0
     for i in string2:
-        if i=="a" or i=="e" or i=="i" or i == "o" or i=="u" and i != " ":
+        if i in ('a','e','i','o','u') or i in ('A','E','I','O','U'):
             count = count + 1
     return count
 
 
+#consonants
 def numberConsonants (string):
     string3 = string.strip()
     count = 0
-    for i in string3:
-        if i !="0" != "1" != "2" != "3" != "4" != "5" != "6"  != "7" != "8" != "9" !="a" != "e" != "i" != "o" !="u":
+    for i in string3: 
+        if i in ('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
             count = count + 1
-    return count
+        elif i in ('B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z'):
+            count = count + 1
+    return count 
+
 
 words = numberWords (sentence)
 vowels = numberVowels (sentence)
@@ -50,7 +58,3 @@ consonants = numberConsonants (sentence)
 
 display (words, vowels, consonants)
 
-# words = numberWords (sentence)
-# vowels = numberVowels (sentence)
-# consonants = numberConsonants (sentence)
-# display (words, vowels, consonants)
